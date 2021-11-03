8194ac5b662a24868a3c2b1f36b241332e8f5665
Юлия Дмитренко <71491207+dmitrenkoyi@users.noreply.github.com>
2021-11-03 20:31:42 +0200

    fix

--- src/views/HomeViews.js
+++ src/views/HomeViews.js
@@ -1,7 +1,13 @@
+const styles = {
+  headerHomePage: {
+    fontSize: '40px',
+  },
+};
+
 export default function HomePage() {
   return (
     <div>
-      <h1>
+      <h1 style={styles.headerHomePage}>
         Greetings, this app is a phone book. Here you can register and add your
         contacts
       </h1>
